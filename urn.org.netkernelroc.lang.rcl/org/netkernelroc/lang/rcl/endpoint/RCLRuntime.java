841f11d6d22589ac830f7ba95e9af79554fdfb62
Randolph Kahle <randy@databliss.net>
2011-05-04 04:52:02 -0400

    Remove imports of org.w3c.dom package. Change to Java 5 idioms Refactor

--- urn.org.netkernelroc.lang.rcl/org/netkernelroc/lang/rcl/endpoint/RCLRuntime.java
+++ urn.org.netkernelroc.lang.rcl/org/netkernelroc/lang/rcl/endpoint/RCLRuntime.java
@@ -1,5 +1,9 @@
 package org.netkernelroc.lang.rcl.endpoint;
 
+import nu.xom.Document;
+import nu.xom.Element;
+import nu.xom.Elements;
+import nu.xom.Node;
 import nu.xom.converters.DOMConverter;
 import org.netkernel.layer0.nkf.INKFLocale;
 import org.netkernel.layer0.nkf.INKFRequest;
@@ -9,8 +13,6 @@ import org.netkernel.layer0.util.XMLUtils;
 import org.netkernel.module.standard.endpoint.StandardAccessorImpl;
 import org.netkernel.util.Utils;
 import org.w3c.dom.DOMImplementation;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -37,12 +39,12 @@ public class RCLRuntime extends StandardAccessorImpl
     boolean tolerant = context.getThisRequest().argumentExists("tolerant");
     String mimeType = context.getThisRequest().getArgumentValue("mimeType");
 
-    Node templateNode = context.source("arg:template", Node.class);
+    org.w3c.dom.Node templateNode = context.source("arg:template", org.w3c.dom.Node.class);
     // Make a copy so we don't change an immutable resource representation
-    Document template = getMutableClone(templateNode);
+    org.w3c.dom.Document template = getMutableClone(templateNode);
 
-    nu.xom.Document document = DOMConverter.convert(template);
-    nu.xom.Element element = document.getRootElement();
+    Document document = DOMConverter.convert(template);
+    Element element = document.getRootElement();
 
 
     processTemplate(element, context, tolerant);
@@ -71,72 +73,38 @@ public class RCLRuntime extends StandardAccessorImpl
 
   //========== Protected ==========
 
-  protected void processTemplate(nu.xom.Element element, INKFRequestContext context, boolean tolerant) throws Exception
+  protected void processTemplate(Element element, INKFRequestContext context, boolean tolerant) throws Exception
     {
+    ArrayList<Node> replacementNodes = new ArrayList<Node>();
+
     element.removeNamespaceDeclaration("rcl");
 
-    // Gather all of the children of the passed Element, put them
-    // into an array and remove them from the passed Element.
-    ArrayList<nu.xom.Element> childElements = new ArrayList<nu.xom.Element>();
-    nu.xom.Elements elements = element.getChildElements();
-    for (int i=0; i<elements.size(); i++)
+    ArrayList<Element> childElements = stripAndReturnChildElements(element, context, tolerant);
+
+    for (Element e : childElements)
       {
-      nu.xom.Element ee = elements.get(i);
-      if (ee.getChildElements().size() > 0 && ee.getNamespaceURI("rcl")==null)
-        {
-        processTemplate(ee,context, tolerant);
-        }
-      childElements.add(ee);
-      }
-    element.removeChildren();
-
-
-    ArrayList<nu.xom.Node> replacementNodes = new ArrayList<nu.xom.Node>();
-
-
-    // Go through the childElements and process them
-    // into the replacement Elements
-    for (int i = 0; i < childElements.size(); i++)
-      {
-      nu.xom.Element e = childElements.get(i);
-
       if( e.getNamespaceURI("rcl")!= null)
         {
         String name = e.getLocalName();
         if ("include".equals(name))
           {
-          ArrayList<nu.xom.Node> includeNodes = processInclude(e, context, tolerant);
-
-          for( int j=0; j< includeNodes.size(); j++)
-            {
-            replacementNodes.add(includeNodes.get(j));
-            }
-
+          replacementNodes.addAll(processInclude(e, context, tolerant));
           }
         else if ("if".equals(name))
           {
-          ArrayList<nu.xom.Node> includeNodes = processIf(e, context, tolerant);
-
-          for( int j=0; j< includeNodes.size(); j++)
-            {
-            replacementNodes.add(includeNodes.get(j));
-            }
-
+          replacementNodes.addAll(processIf(e, context, tolerant));
           }
         }
       else
         {
-        nu.xom.Element ee = childElements.get(i);
-        replacementNodes.add(ee);
+        replacementNodes.add(e);
         }
-
       }
-    // Now use the replacement nodes to build up the template element
-    for( int i=0; i<replacementNodes.size(); i++)
+
+    for(Node node : replacementNodes)
       {
-      element.appendChild(replacementNodes.get(i));
+      element.appendChild(node);
       }
-
     }
 
 
@@ -151,57 +119,29 @@ public class RCLRuntime extends StandardAccessorImpl
    * @param tolerant Indicates if processing should be tolerant of errors.
    *
    */
-  protected ArrayList<nu.xom.Node> processInclude(nu.xom.Element includeElement, INKFRequestContext context, boolean tolerant) throws Exception
+  protected ArrayList<Node> processInclude(Element includeElement, INKFRequestContext context, boolean tolerant) throws Exception
     {
-    nu.xom.Element newElement;
+    ArrayList<Node> replacementNodes = new ArrayList<Node>();
 
-    // Gather the children of the include element
-    // and remove them from the include element itself
-    ArrayList<nu.xom.Element> childElements = new ArrayList<nu.xom.Element>();
-    nu.xom.Elements elements = includeElement.getChildElements();
-    for (int i=0; i<elements.size(); i++)
+    ArrayList<Element> childElements = stripAndReturnChildElements(includeElement, context, tolerant);
+
+    for(Element e : childElements)
       {
-      nu.xom.Element ee = elements.get(i);
-      if (ee.getChildElements().size() > 0 && ee.getNamespaceURI("rcl")==null)
-        {
-        processTemplate(ee,context, tolerant);
-        }
-
-      childElements.add(ee);
-      }
-    includeElement.removeChildren();
-
-    ArrayList<nu.xom.Node> replacementNodes = new ArrayList<nu.xom.Node>();
-
-
-    for (int i=0; i<childElements.size(); i++)
-      {
-      newElement = null;
-      nu.xom.Element e = elements.get(i);
-
       if( e.getNamespaceURI("rcl")!= null)
          {
          String name = e.getLocalName();
          if ("request".equals(name))
            {
-           newElement = processRequest(e, context);
-           nu.xom.Node nn = newElement.copy();
-           replacementNodes.add(nn);
+           replacementNodes.add(processRequest(e, context).copy());
            }
          if("include".equals(name))
            {
-           ArrayList<nu.xom.Node> includeNodes = processInclude(e, context,  tolerant);
-           for(int j=0; j < includeNodes.size();j++)
-             {
-             replacementNodes.add(includeNodes.get(j));
-             }
+           replacementNodes.addAll(processInclude(e, context,  tolerant));
            }
          }
       else
         {
-        nu.xom.Element ee = childElements.get(i);
-        //processTemplate(ee, context, tolerant);
-        replacementNodes.add(ee);
+        replacementNodes.add(e);
         }
       }
 
@@ -210,21 +150,21 @@ public class RCLRuntime extends StandardAccessorImpl
 
 
 
-  protected nu.xom.Element processRequest(nu.xom.Element requestElement, INKFRequestContext context) throws Exception
+  protected Element processRequest(Element requestElement, INKFRequestContext context) throws Exception
     {
     INKFRequest request = buildRequest(requestElement, context);
     request.setRepresentationClass(org.w3c.dom.Node.class);
-    org.w3c.dom.Node node = (Node)context.issueRequest(request);
+    org.w3c.dom.Node node = (org.w3c.dom.Node)context.issueRequest(request);
 
-    Document template = getMutableClone(node);
+    org.w3c.dom.Document template = getMutableClone(node);
 
-    nu.xom.Document document = DOMConverter.convert(template);
+    Document document = DOMConverter.convert(template);
     return document.getRootElement();
     }
 
 
 
-  protected boolean processRequestForBoolean(nu.xom.Element requestElement, INKFRequestContext context) throws Exception
+  protected boolean processRequestForBoolean(Element requestElement, INKFRequestContext context) throws Exception
     {
     INKFRequest request = buildRequest(requestElement, context);
     request.setRepresentationClass(java.lang.Boolean.class);
@@ -245,11 +185,11 @@ public class RCLRuntime extends StandardAccessorImpl
      *   rcl:representation
      */
 
-    protected INKFRequest buildRequest(nu.xom.Element requestElement, INKFRequestContext context) throws Exception
+    protected INKFRequest buildRequest(Element requestElement, INKFRequestContext context) throws Exception
       {
       INKFRequest request = null;
 
-      nu.xom.Elements elements = requestElement.getChildElements();
+      Elements elements = requestElement.getChildElements();
 
       // We have to know the identifier first
       for (int i=0; i<elements.size(); i++)
@@ -267,70 +207,39 @@ public class RCLRuntime extends StandardAccessorImpl
 
 
 
-  protected ArrayList<nu.xom.Node> processIf(nu.xom.Element ifElement, INKFRequestContext context, boolean tolerant) throws Exception
+  protected ArrayList<Node> processIf(Element ifElement, INKFRequestContext context, boolean tolerant) throws Exception
     {
-    nu.xom.Element newElement = null;
+    ArrayList<Node> replacementNodes = new ArrayList<Node>();
     boolean test = false;
 
-    // Gather the children of the if element
-    // and remove them from the if element itself
-    ArrayList<nu.xom.Element> childElements = new ArrayList<nu.xom.Element>();
-    nu.xom.Elements elements = ifElement.getChildElements();
-    for (int i=0; i<elements.size(); i++)
+    ArrayList<Element> childElements = stripAndReturnChildElements(ifElement, context, tolerant);
+
+    // Find and issue the request
+    for(Element e : childElements)
       {
-      nu.xom.Element ee = elements.get(i);
-      if (ee.getChildElements().size() > 0 && ee.getNamespaceURI("rcl")==null)
-        {
-        processTemplate(ee,context, tolerant);
-        }
-
-      childElements.add(ee);
-      }
-    ifElement.removeChildren();
-
-
-    ArrayList<nu.xom.Node> replacementNodes = new ArrayList<nu.xom.Node>();
-
-    // Find the request node, issue the request and find out which sub-node fills the replacementNodes collection
-    for (int i=0; i<childElements.size(); i++)
-      {
-      nu.xom.Element e = childElements.get(i);
-      if( e.getNamespaceURI("rcl")!= null)
+      if( e.getNamespaceURI("rcl")!= null && "request".equals(e.getLocalName()))
          {
-         String name = e.getLocalName();
-         if ("request".equals(name))
-           {
-           test = processRequestForBoolean(e, context);
-           }
+         test = processRequestForBoolean(e, context);
          }
       }
 
-     for (int i = 0; i< childElements.size(); i++)
-       {
-       nu.xom.Element e = childElements.get(i);
+    for(Element e : childElements)
+      {
        if (e.getNamespaceURI("rcl")!=null)
          {
          if ("true".equals(e.getLocalName()) && test)
            {
-           ArrayList<nu.xom.Node> includeNodes = processIfTrueFalse(e, context,  tolerant);
-           for(int j=0; j < includeNodes.size();j++)
-             {
-             replacementNodes.add(includeNodes.get(j));
-             }
+           replacementNodes.addAll(processIfTrueFalse(e, context, tolerant));
            }
          if ("false".equals(e.getLocalName()) && !test)
            {
-           ArrayList<nu.xom.Node> includeNodes = processIfTrueFalse(e, context,  tolerant);
-           for(int j=0; j < includeNodes.size();j++)
-             {
-             replacementNodes.add(includeNodes.get(j));
-             }
+           replacementNodes.addAll(processIfTrueFalse(e, context, tolerant));
            }
          // We also need if and include here...
          }
        else
          {
-         replacementNodes.add(childElements.get(i));
+         replacementNodes.add(e);
          }
        }
 
@@ -338,28 +247,14 @@ public class RCLRuntime extends StandardAccessorImpl
     }
 
 
-  protected ArrayList<nu.xom.Node> processIfTrueFalse(nu.xom.Element trueFalseElement, INKFRequestContext context, boolean tolerant) throws Exception
+  protected ArrayList<Node> processIfTrueFalse(Element trueFalseElement, INKFRequestContext context, boolean tolerant) throws Exception
     {
-    ArrayList<nu.xom.Element> childElements = new ArrayList<nu.xom.Element>();
-    nu.xom.Elements elements = trueFalseElement.getChildElements();
-    for (int i=0; i<elements.size(); i++)
+    ArrayList<Node> replacementNodes = new ArrayList<Node>();
+
+    ArrayList<Element> childElements = stripAndReturnChildElements(trueFalseElement, context, tolerant);
+
+    for(Element e: childElements)
       {
-      nu.xom.Element ee = elements.get(i);
-      if (ee.getChildElements().size() > 0 && ee.getNamespaceURI("rcl")==null)
-        {
-        processTemplate(ee,context, tolerant);
-        }
-
-      childElements.add(ee);
-      }
-    trueFalseElement.removeChildren();
-
-
-    ArrayList<nu.xom.Node> replacementNodes = new ArrayList<nu.xom.Node>();
-
-    for (int i = 0; i< childElements.size(); i++)
-      {
-      nu.xom.Element e = childElements.get(i);
       if (e.getNamespaceURI("rcl")!=null)
         {
         // For now we will ignore rcl: sub-elements
@@ -367,13 +262,30 @@ public class RCLRuntime extends StandardAccessorImpl
         }
       else
         {
-        replacementNodes.add(childElements.get(i));
+        replacementNodes.add(e);
         }
       }
 
    return replacementNodes;
+    }
 
+  protected ArrayList<Element> stripAndReturnChildElements(Element element, INKFRequestContext context, boolean tolerant ) throws Exception
+    {
+    ArrayList<Element> childElements = new ArrayList<Element>();
+    Elements elements = element.getChildElements();
+    for (int i=0; i<elements.size(); i++)
+      {
+      Element e = elements.get(i);
+      if (e.getChildElements().size() > 0 && e.getNamespaceURI("rcl")==null)
+        {
+        processTemplate(e, context, tolerant);
+        }
 
+      childElements.add(e);
+      }
+    element.removeChildren();
+
+    return childElements;
     }
 
 
@@ -382,12 +294,12 @@ public class RCLRuntime extends StandardAccessorImpl
 
 
 
-  private Document getMutableClone(Node node) throws ParserConfigurationException
+  private org.w3c.dom.Document getMutableClone(org.w3c.dom.Node node) throws ParserConfigurationException
     {
-    Document result;
-    if (node instanceof Document)
+    org.w3c.dom.Document result;
+    if (node instanceof org.w3c.dom.Document)
       {
-      result = (Document) XMLUtils.safeDeepClone(node);
+      result = (org.w3c.dom.Document) XMLUtils.safeDeepClone(node);
       }
     else
       {
@@ -398,7 +310,7 @@ public class RCLRuntime extends StandardAccessorImpl
     }
 
 
-  private void exceptionHandler(INKFRequestContext aHelper, boolean tolerant, String aException, String aMessage, Node aElement, Exception e, String target) throws Exception
+  private void exceptionHandler(INKFRequestContext aHelper, boolean tolerant, String aException, String aMessage, org.w3c.dom.Node aElement, Exception e, String target) throws Exception
     {
     if (tolerant)
       {
